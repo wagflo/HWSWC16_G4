de32375c5f0d6b9066addffa87518b2263565913
Jeanne Phi <horizontalwinkel@gmail.com>
2017-02-23 15:08:40 +0100

    safety commit 002

--- template/vhdl/components/rayDelay.vhd
+++ template/vhdl/components/rayDelay.vhd
@@ -7,6 +7,13 @@ use work.operations_pkg.all;
 
 entity rayDelay is 
 generic (DELAY_LENGTH : NATURAL := 1);
-port (inputRay : in ray; outputRay : out ray);
+port (clk, reset, clk_en : in std_logic;
+inputRay : in ray; outputRay : out ray);
 end entity;
 
+architecture arch of rayDelay is
+signal subwire0, subwire1 : std_logic_vector(95 downto 0);
+begin
+delay : delay_element generic map (DEPTH => DELAY_LENGTH, WIDTH => 96)
+port map (clk => clk, reset => reset, clken => clk_en, source => subwire0, dest => subwire1);
+end architecture;
