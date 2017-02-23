783d77e9048e72353ac81186d650f2b97e287bd4
Jeanne Phi <horizontalwinkel@gmail.com>
2017-02-23 14:53:52 +0100

    better modularisation

---
+++ template/vhdl/components/rayDelay.vhd
@@ -0,0 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+use work.delay_pkg.all;
+
+use work.operations_pkg.all;
+
+entity rayDelay is 
+generic (DELAY_LENGTH : NATURAL := 1);
+port (inputRay : ray )
