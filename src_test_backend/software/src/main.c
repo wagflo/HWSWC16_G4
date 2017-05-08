dfe28a77572607090bd2476dbf3f5161388368b0
Michael Kashofer <michael.kashofer@gmx.at>
2017-05-08 19:51:08 +0200

    testing the backend on board

---
+++ src_test_backend/software/src/main.c
@@ -0,0 +1,159 @@
+/*******************************************************************************
+ * @file main.c
+ * @brief Main file for the HWSW 2016 main task.
+ *
+ * Please use the given functions 'init', 'setCamera' and 'renderFrame' for
+ * your project and do not forget to comment your code!
+ *
+ * To get started, the module 'sw_renderer' provides a simple software-only
+ * raytracer. A testscene is given in 'testdata'.
+ ******************************************************************************/
+
+/* Includes *******************************************************************/
+#include "defs.h"
+#include "vec3.h"
+#include "sphere.h"
+#include "sw_renderer.h"
+#include "testdata.h"
+#include "display.h"
+
+#include <stdint.h>
+
+#include <stdio.h>
+#include <io.h>
+#include <malloc.h>
+#include <system.h>
+
+#include <sys/alt_timestamp.h>
+#include <altera_avalon_timer_regs.h>
+
+/* Defines ********************************************************************/
+/* Typedefs *******************************************************************/
+/* Variables ******************************************************************/
+/* Function prototypes ********************************************************/
+/** 
+ * @brief Setup hardware
+ */
+static void setupHW (void);
+
+/** 
+ * @brief Initialize raytracer
+ * 
+ * @param num_objects  Number of spheres
+ * @param spheres      Sphere array
+ * @param max_reflects Maximal number of reflects
+ * @param num_samples  Number of samples per pixel
+ */
+static void init (
+		uint8_t num_objects, sphere_t *spheres,
+		uint16_t max_reflects, uint16_t num_samples);
+
+/** 
+ * @brief Set camera parameters
+ * 
+ * @param lookfrom Observer position
+ * @param lookat   Observer direction
+ * @param vfov     Vertical field of view in degrees
+ */
+static void setCamera (vec3_t *lookfrom, vec3_t *lookat, fix16_t vfov, uint8_t frame_address);
+
+/** 
+ * @brief Start raytracer
+ */
+static void renderFrame (void);
+
+/* main ***********************************************************************/
+int
+main (void)
+{
+	printf("Vor setupHW\n");
+	setupHW ();
+	printf("Nach setupHW\n");
+	uint8_t fb = 0;
+	uint8_t start = 0;
+	selectFramebuffer (fb);
+	displayClear (0);
+	uint8_t modulo = 0;
+	uint32_t color_x = 0x00000000;
+	uint32_t color_y = 0x00000000;
+	uint32_t color_z = 0x00000000;
+	while (1)
+	{
+	  if (start > 0) {
+	    uint32_t stall = IORD(MM_INTERFACE_TEST_0_BASE, 0x0001);
+	    uint32_t position = IORD(MM_INTERFACE_TEST_0_BASE, 0x0002);
+	    printf("         \n");
+	    uint32_t counter0 = IORD(MM_INTERFACE_TEST_0_BASE, 0x0003);
+	    printf("Stall: %x, position: %x, counter0: %d\n", stall, position, counter0);
+	    while (IORD(MM_INTERFACE_TEST_0_BASE, 0x0000) == 0x00000000) {
+	      stall = IORD(MM_INTERFACE_TEST_0_BASE, 0x0001);
+	      position = IORD(MM_INTERFACE_TEST_0_BASE, 0x0002);
+	      counter0 = IORD(MM_INTERFACE_TEST_0_BASE, 0x0003);
+	      //printf("Stall: %x, position: %x, counter0: %d\n", stall, position, counter0);
+	    }
+	    for (int i = 0; i < 1e4; ++i) {
+	      printf(" ");
+	    }
+	    printf("\nDONE\n");
+	  }
+	  else {
+	    start++;
+	  }
+	  showFramebuffer(fb);
+	  fb ^= 0x01;
+	  uint32_t address = getAddress(fb);
+	  IOWR(MM_INTERFACE_TEST_0_BASE, 0x0002, address);
+	  modulo = (modulo + 1) % 3;
+	  if (modulo == 0) {
+	    color_x = 0x00010000;
+	    color_y = 0x00000000;
+	    color_z = 0x00000000;
+	  } else if (modulo == 1) {
+	    color_x = 0x00000000;
+	    color_y = 0x00010000;
+	    color_z = 0x00000000;
+	  } else {
+	    color_x = 0x00000000;
+	    color_y = 0x00000000;
+	    color_z = 0x00010000;
+	  }
+	  IOWR(MM_INTERFACE_TEST_0_BASE, 0x0003, color_x);
+	  IOWR(MM_INTERFACE_TEST_0_BASE, 0x0003, color_y);
+	  IOWR(MM_INTERFACE_TEST_0_BASE, 0x0003, color_z);
+	  IOWR(MM_INTERFACE_TEST_0_BASE, 0xFFFF, 0x00000000);
+	}
+	return 0;
+}
+
+/* Private functions **********************************************************/
+static void
+setupHW (void)
+{
+	IOWR_ALTERA_AVALON_TIMER_STATUS(SYSTIMER_BASE, 0); // clear TO bit
+	/* display init */
+	/* allocate memory for 2 frame buffers (double buffering)*/
+	void *framebuffers = malloc (FRAME_HEIGHT * FRAME_WIDTH * sizeof (uint32_t) * 2);
+	displayInit ((uint32_t) framebuffers,
+		(uint32_t) framebuffers + FRAME_HEIGHT * FRAME_WIDTH * sizeof (uint32_t));
+	// HW Reset hopefully
+}
+
+static void
+init (
+	uint8_t num_objects, sphere_t *spheres,
+	uint16_t max_reflects, uint16_t num_samples)
+{
+	rtInit (num_objects, spheres, max_reflects, num_samples);
+}
+
+static void
+setCamera (vec3_t *lookfrom, vec3_t *lookat, fix16_t vfov, uint8_t frame_address)
+{
+	rtSetCamera (lookfrom, lookat, vfov, frame_address);
+}
+
+static void
+renderFrame (void)
+{
+	rtRenderFrame ();
+}
---
+++ src_test_backend/vhdl/tb_michael/readInterfaceTest.vhd
@@ -0,0 +1,189 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use work.components_pkg.all;
+use work.operations_pkg.all;
+
+entity readIFTest is
+
+end entity;
+
+architecture arch of readIFTest is
+
+constant PIPEMAX : integer := 13; -- how much can be in pipeline
+
+constant STARTOFREAD : natural := 25;
+
+constant RAYPERIOD : integer := 1;
+constant RAYCYCLESACTIVE : integer := 1;
+constant FIFOSIZE : integer := 5;
+
+signal pipeline : std_logic_vector(PIPEMAX - 1 downto 0) := (others => '0');
+
+signal clk : std_logic := '1';
+signal res : std_logic := '1';
+signal valid_t : std_logic := '0';
+
+-- BEWARE OF LONG INIT/RESET PHASE: 17 clocks
+
+type data_array is array(natural range <>) of std_logic_vector(7 downto 0);
+
+signal data : data_array(14 downto 0) := (
+	-- sob, eob, valid_ray, valid_t, emitting, remaining
+	0 => "10100111", -- no refl (no valid_t)			nr p
+	1 => "01110111", -- refl => all pseudo				r  p
+	2 => "00000001",	--					0  0
+	3 => "00000010",	--					0  0	
+	4 => "00000100", --						0  0
+	5 => "10110000", --						nr np 
+	6 => "01110000", -- no remaining => no pseudo => no refl	nr np
+	7 => "10111010", -- 						nr np
+	8 => "01111010", -- valid_t but, emitting => no pseudo, no refl	nr np
+	9 => "00000000", --						0  0   => kleiner Fehler, aber ausserhalb von sob, eob
+	10=> "00000000", --						0  0
+	11=> "10110010", -- refl					r  p
+	12=> "01111010", -- only one emitting => pseudo                 nr p
+	13=> "10101010", -- 						nr np
+	14=> "01101010"); -- no refl, just emitting => no pseudo	nr np
+
+signal input : std_logic_vector(31 downto 0) := x"5a_6c_5c_6a"; -- := "0000000";
+signal output1, output2 : std_logic_vector(31 downto 0);
+
+signal counter : integer := 0;
+signal pipeline_counter : integer := PIPEMAX;
+signal slave_counter : integer := 0;
+
+signal input_address 	: std_logic_vector(31 downto 0);
+signal input_color 	: std_logic_vector(23 downto 0);
+signal input_valid 	: std_logic;
+signal output_address	: std_logic_vector(31 downto 0);
+signal output_color 	: std_logic_vector(31 downto 0);
+signal read_req 	: std_logic;
+
+signal stall : std_logic;
+
+signal waitreq : std_logic := '0';
+
+function to_std_logic(input : boolean) return std_logic is
+begin
+if input then
+  return '1';
+else
+  return '0';
+end if;
+end function;
+
+
+function rayOrNot(count : integer; period : integer; active : integer) return std_logic is
+begin
+
+return to_std_logic(count mod period > active);
+
+end function;
+
+begin
+
+dut : readInterface
+  generic map
+  (
+    FIFOSIZE => FIFOSIZE
+  )
+  port map
+  (
+    clk 	=> clk,
+    clk_en 	=> '1',
+    reset 	=> res,
+   
+    -- kein clock enable, nehme valid
+
+    pixel_address => input_address,
+    pixel_color   => input_color,
+    valid_data    => pipeline(PIPEMAX - 1), --input_valid,
+
+    stall 	  => stall,
+    
+    slave_address  	=> output_address,
+    slave_data		=> output_color,
+    slave_read   	=> read_req
+
+    --slave_waitreq	=> waitreq
+  );
+
+clk <= not clk after 10 ns;
+
+res <= '0' after 425 ns;
+
+--input <= not input after 10 ns;
+
+sync : process(clk, res) 
+begin
+
+if res = '1' then
+
+  input_address <= (others => '0');
+  input_color   <= (others => '0');
+  input_valid   <= '0';
+  pipeline 	<= (others => '0');
+
+elsif rising_edge(clk) then
+
+  pipeline(0) <= not stall and rayOrNot(slave_counter, RAYPERIOD, RAYCYCLESACTIVE);
+  pipeline(PIPEMAX - 1 downto 1) <= pipeline(PIPEMAX - 2 downto 0);
+
+  slave_counter <= slave_counter + 1;
+
+  if stall = '0' and pipeline_counter < PIPEMAX then
+    pipeline_counter <= pipeline_counter + 1;
+  elsif stall = '1' and pipeline_counter > 0 then
+    pipeline_counter <= pipeline_counter - 1;
+  else 
+    pipeline_counter <= pipeline_counter;
+  end if;
+
+  if pipeline_counter > 0 then
+    counter <= counter + 1;
+    input_valid <= '1';
+  else
+    counter <= counter;
+    input_valid <= '0';
+  end if;
+  
+  input_address <= std_logic_vector(to_signed(counter, 32));
+  input_color <= std_logic_vector(to_signed(counter, 24));
+
+end if;
+
+end process;
+
+read_req <= to_std_logic((slave_counter >= STARTOFREAD) and (slave_counter mod 5) = 0);
+
+
+--input <= data(counter);
+--sob <= input(7);
+--eob <= input(6);
+--valid_ray <= input(5);
+--valid_t <= input(4);
+--emitting <= input(3);
+--remaining <= input(2 downto 0);
+
+--counting : process(clk, res)
+--begin
+--if res = '1' then
+ 
+--  input <= "0000000";
+--  counter <= 0;
+--els
+--if rising_edge(clk) then
+  --input <= data(counter);
+--  counter <= counter + 1;
+--  if counter = data'length - 1 then
+--    counter <= 0;
+--  end if;
+--end if;
+--end process;
+
+
+
+
+
+end architecture;
